5dfda72e670cd41b07614dc58b1a6b0445bdcbd4
svoh86 <sms-86@mail.ru>
2022-02-06 13:10:39 +0300

    Переопределение [#174890 #260738]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,6 +4,9 @@ public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return "{" + System.lineSeparator() + "\t\"name\" " + ": " + "\"" + name + "\"" + "," + System.lineSeparator() + "\t\"body\" " + ": " + "\"" + body + "\"" + System.lineSeparator() + "}";
+        String ln = System.lineSeparator();
+        return "{" + ln
+                + "\t\"name\" " + ": " + "\"" + name + "\"" + "," + ln
+                + "\t\"body\" " + ": " + "\"" + body + "\"" + ln + "}";
     }
 }
