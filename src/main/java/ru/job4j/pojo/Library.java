bb710feccdca1523cd5bec2f3efb1590845e21e0
svoh86 <sms-86@mail.ru>
2022-02-07 08:57:21 +0300

    Массивы и модели. [#174892 #261095]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -25,7 +25,7 @@ public class Library {
             System.out.println(b.getName() + " " + b.getPages());
         }
         for (int i = 0; i < books.length; i++) {
-            if (books[i].getName().equals("Clean code")) {
+            if ("Clean code".equals(books[i].getName())) {
                 System.out.println(books[i].getName());
             }
         }
