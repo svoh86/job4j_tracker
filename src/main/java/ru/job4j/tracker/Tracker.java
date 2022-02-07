fd432511e248df38755d064d5520270b1e3c43f4
svoh86 <sms-86@mail.ru>
2022-02-07 10:28:07 +0300

    Tracker - хранилище [#396 #261189]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -19,16 +19,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                rsl[count] = items[i];
-                count++;
-            }
-        }
-        rsl = Arrays.copyOf(rsl, count);
-        return rsl;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
