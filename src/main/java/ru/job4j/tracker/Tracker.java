9a257aeb2a9ac929a0a84d9de9d865d1163c557a
svoh86 <sms-86@mail.ru>
2022-02-07 09:13:08 +0300

    Метод замены заявки. Tracker.replace [#211748 #261204]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,9 +56,10 @@ public class Tracker {
     }
 
     public boolean replace(int id, Item item) {
-        if (indexOf(id) != -1) {
+        int index = indexOf(id);
+        if (index != -1) {
             item.setId(id);
-            items[indexOf(id)] = item;
+            items[index] = item;
             return true;
         }
         return false;
