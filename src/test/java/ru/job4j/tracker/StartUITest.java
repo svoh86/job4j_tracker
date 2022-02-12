32e2fd33e3a628c127b6506ea19f02dc0e9f3bf5
svoh86 <sms-86@mail.ru>
2022-02-12 17:24:38 +0300

    Реализация меню за счет шаблона стратегия. [#181780]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,37 +6,4 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class StartUITest {
-
-    @Test
-    public void whenAddItem() {
-        String[] answer = {"Fix PC"};
-        Input input = new StubInput(answer);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-
-    }
-
-    @Test
-    public void whenEditItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answer = {String.valueOf(item.getId()), "edit item"};
-        StartUI.editItem(new StubInput(answer), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName(), is("edit item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item();
-        tracker.add(item);
-        String[] answer = {String.valueOf(item.getId())};
-        StartUI.deleteItem(new StubInput(answer), tracker);
-        assertNull(tracker.findById(item.getId()));
-    }
 }
