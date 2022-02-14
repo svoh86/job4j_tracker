027f60e3498a622ac813c2959a61d78cf822d239
svoh86 <sms-86@mail.ru>
2022-02-14 11:40:12 +0300

    Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -15,37 +15,118 @@ public class StartUITest {
         UserAction[] actions = {new CreateAction(), new ExitAction()};
         new StartUI().init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("New item"));
-    }
+    }*/
 
     @Test
-    public void whenReplaceItem() {
+    public void whenReplaceItemTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
         Input in = new StubInput(new String[]{"0", String.valueOf(item.getId()), replacedName, "1"});
-        UserAction[] actions = {new EditAction(), new ExitAction()};
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+        UserAction[] actions = {new EditAction(output), new ExitAction(output)};
+        String ln = System.lineSeparator();
+        new StartUI(output).init(in, tracker, actions);
+        assertThat(output.toString(), is(
+                "Menu." + ln
+                        + "0. Edit item" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Edit item ===" + ln
+                        + "Заявка изменена успешно." + ln
+                        + "Menu." + ln
+                        + "0. Edit item" + ln
+                        + "1. Exit Program" + ln
+                        + "===Exit Program===" + ln
+        ));
     }
 
     @Test
-    public void whenReplaceItemVarTwo() {
+    public void whenShowAllItemsTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
         Tracker tracker = new Tracker();
-        Input in = new StubInput(new String[]{"0", "New item", "1", "1", "Edit item", "2"});
-        UserAction[] actions = {new CreateAction(), new EditAction(), new ExitAction()};
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findAll()[0].getName(), is("Edit item"));
+        Item item = tracker.add(new Item("New item"));
+        Input in = new StubInput(new String[]{"0", "1"});
+        UserAction[] actions = {new ShowAllAction(output), new ExitAction(output)};
+        String ln = System.lineSeparator();
+        new StartUI(output).init(in, tracker, actions);
+        assertThat(output.toString(), is(
+                "Menu." + ln
+                        + "0. Show all items" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Show all items ===" + ln
+                        + item + ln
+                        + "Menu." + ln
+                        + "0. Show all items" + ln
+                        + "1. Exit Program" + ln
+                        + "===Exit Program===" + ln
+        ));
     }
 
     @Test
-    public void whenDeleteItem() {
+    public void whenFindByNameTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("New"));
+        Input in = new StubInput(new String[]{"0", item.getName(), "1"});
+        UserAction[] actions = {new FindByNameAction(output), new ExitAction(output)};
+        String ln = System.lineSeparator();
+        new StartUI(output).init(in, tracker, actions);
+        assertThat(output.toString(), is(
+                "Menu." + ln
+                        + "0. Find items by name" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Find items by name ===" + ln
+                        + item + ln
+                        + "Menu." + ln
+                        + "0. Find items by name" + ln
+                        + "1. Exit Program" + ln
+                        + "===Exit Program===" + ln
+        ));
+    }
+
+    @Test
+    public void whenFindByIdTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("New"));
+        Input in = new StubInput(new String[]{"0", String.valueOf(item.getId()), "1"});
+        UserAction[] actions = {new FindByIdAction(output), new ExitAction(output)};
+        String ln = System.lineSeparator();
+        new StartUI(output).init(in, tracker, actions);
+        assertThat(output.toString(), is(
+                "Menu." + ln
+                        + "0. Find item by id" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Find item by id ===" + ln
+                        + item + ln
+                        + "Menu." + ln
+                        + "0. Find item by id" + ln
+                        + "1. Exit Program" + ln
+                        + "===Exit Program===" + ln
+        ));
+    }
+
+    @Test
+    public void whenDeleteItemOutputIsSuccessfully() {
+        Output output = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(new String[]{"0", String.valueOf(item.getId()), "1"});
-        UserAction[] actions = {new DeleteAction(), new ExitAction()};
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()), is(nullValue()));
-    }*/
+        UserAction[] actions = {new DeleteAction(output), new ExitAction(output)};
+        String ln = System.lineSeparator();
+        new StartUI(output).init(in, tracker, actions);
+        assertThat(output.toString(), is(
+                "Menu." + ln
+                        + "0. Delete item" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Delete item ===" + ln
+                        + "Заявка удалена успешно." + ln
+                        + "Menu." + ln
+                        + "0. Delete item" + ln
+                        + "1. Exit Program" + ln
+                        + "===Exit Program===" + ln
+        ));
+    }
 
     @Test
     public void whenExit() {
