cc66d476d5b8d273de1ff9d65d2a574a55669a89
svoh86 <sms-86@mail.ru>
2022-02-15 12:28:49 +0300

    Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.*;
 
 public class StartUITest {
@@ -138,5 +137,22 @@ public class StartUITest {
         assertThat(out.toString(), is("Menu." + System.lineSeparator()
                 + "0. Exit Program" + System.lineSeparator()
                 + "===Exit Program===" + System.lineSeparator()));
+    }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"8", "0"});
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {new ExitAction(out)};
+        String ln = System.lineSeparator();
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                "Menu." + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu." + ln
+                        + "0. Exit Program" + ln
+                        + "===Exit Program===" + ln));
     }
 }
