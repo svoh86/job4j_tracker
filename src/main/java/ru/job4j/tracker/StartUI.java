124d04906eee64184dfabcf59077a6bc8d3ce88c
svoh86 <sms-86@mail.ru>
2022-02-15 12:18:51 +0300

    Обеспечить бесперебойную работу приложения Tracker [#789]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,12 +12,12 @@ public class StartUI {
         while (run) {
             this.showMenu(actions);
             int select = input.askInt("Select: ");
-            if (select >= 0 && select <= 6) {
-                UserAction action = actions[select];
-                run = action.execute(input, tracker);
-            } else {
-                out.println("Select from 0 to 6");
+            if (select < 0 || select >= actions.length) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.length - 1));
+                continue;
             }
+            UserAction action = actions[select];
+            run = action.execute(input, tracker);
         }
     }
 
@@ -30,7 +30,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {new CreateAction(output), new ShowAllAction(output), new EditAction(output),
                 new DeleteAction(output), new FindByIdAction(output), new FindByNameAction(output),
