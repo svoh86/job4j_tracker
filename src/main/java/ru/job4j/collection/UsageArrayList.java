da7db4741088022bf2f9c8a51911ef4c9bd7dd3a
svoh86 <sms-86@mail.ru>
2022-02-22 15:02:34 +0300

    Коллекции, ArrayList, List, Обобщения [#10094 #268092]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String > names = new ArrayList<>();
+        ArrayList<String> names = new ArrayList<>();
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
