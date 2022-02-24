cc1f0c6db29b675b65d013ab41d28a8a4a6a251d
svoh86 <sms-86@mail.ru>
2022-02-24 12:20:10 +0300

    Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String s : origin) {
+            check.add(s);
+        }
+        for (String t : text) {
+            if (!check.contains(t)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
