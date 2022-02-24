6a317509dd4bd3081ba92bc476c4d2f345ccd0e3
svoh86 <sms-86@mail.ru>
2022-02-24 11:53:27 +0300

    Уникальные задачи. [#212653 #268922]

--- src/main/java/ru/job4j/collection/Task.java
+++ src/main/java/ru/job4j/collection/Task.java
@@ -4,7 +4,6 @@ public class Task {
     private String number;
     private String description;
 
-
     public Task(String number, String description) {
         this.number = number;
         this.description = description;
