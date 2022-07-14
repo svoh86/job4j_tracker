652fbdcc9c23a21f849473475729647fcce342e9
svoh86 <sms-86@mail.ru>
2022-07-14 10:29:44 +0300

    рефакторинг

--- src/main/java/ru/job4j/tracker/MemTracker.java
+++ src/main/java/ru/job4j/tracker/MemTracker.java
@@ -7,21 +7,25 @@ public class MemTracker implements Store {
     private final List<Item> items = new ArrayList<>();
     private int ids = 1;
 
+    @Override
     public Item add(Item item) {
         item.setId(ids++);
         items.add(item);
         return item;
     }
 
+    @Override
     public Item findById(int id) {
         int index = indexOf(id);
         return index != -1 ? items.get(index) : null;
     }
 
+    @Override
     public List<Item> findAll() {
         return List.copyOf(items);
     }
 
+    @Override
     public List<Item> findByName(String key) {
         List<Item> rsl = new ArrayList<>();
         for (Item item : items) {
@@ -43,6 +47,7 @@ public class MemTracker implements Store {
         return rsl;
     }
 
+    @Override
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
@@ -53,6 +58,7 @@ public class MemTracker implements Store {
         return false;
     }
 
+    @Override
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
