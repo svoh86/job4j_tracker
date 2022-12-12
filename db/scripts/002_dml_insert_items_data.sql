83d4d89060ceae714c92a6969e0a1ca351ed600f
svoh86 <sms-86@mail.ru>
2022-12-12 15:18:17 +0300

    2. HQL [#6874 #369766]

---
+++ db/scripts/002_dml_insert_items_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO items (name, created) VALUES ('Item 1', '2022-05-16 15:58:11.929188');
+INSERT INTO items (name, created) VALUES ('Item 2', '2023-05-16 15:58:11.929188');
+INSERT INTO items (name, created) VALUES ('Item 3', '2024-05-16 15:58:11.929188');
